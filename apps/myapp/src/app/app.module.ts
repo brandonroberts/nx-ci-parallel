be8f2772b79f949987689d46dfceaa27eea87b0c
Brandon Roberts <brandon@nrwl.io>
2020-05-29 13:23:13 -0500

    add lib

--- apps/myapp/src/app/app.module.ts
+++ apps/myapp/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MyLibModule } from '@nx-ci-parallel/my-lib';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MyLibModule],
   providers: [],
   bootstrap: [AppComponent],
 })
