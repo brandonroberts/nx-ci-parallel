5759aed8f4a32ff337538a8e126f284d1db1fa74
Brandon Roberts <brandon@nrwl.io>
2020-05-29 15:18:48 -0500

    libs

--- libs/my-lib3/src/lib/my-lib3.module.ts
+++ libs/my-lib3/src/lib/my-lib3.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MyLib2Module } from '@nx-ci-parallel/my-lib2';
+import { MyLibModule } from '@nx-ci-parallel/my-lib';
+import { MyLib4Module } from '@nx-ci-parallel/my-lib4';
 
 @NgModule({
-  imports: [CommonModule, MyLib2Module],
+  imports: [CommonModule, MyLib2Module, MyLibModule, MyLib4Module],
 })
 export class MyLib3Module {}
